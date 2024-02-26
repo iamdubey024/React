0bc3f0c4a2f5151b19d5e51e1f254ba5d9d5e6a4
Ayush Dubey <ayushdubey0806@gmail.com>
2024-02-26 14:53:25 +0530

    learn to create our own react library

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,7 +1,17 @@
 import Chai from "./chai";
 
 function App() {
-  return (<Chai />);
+
+  // use Javascript in React
+
+  const username = "I got an Internship"
+  return (
+  
+  <>
+  <Chai />
+  <p>Chai aur React karne ke baad {username}</p>     {/* {final outcomes}*/}
+  </>
+  );
 }
 
 export default App;
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -1,10 +1,39 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>My name is Ayush</p>
+    </div>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
- 
+// react doesn't understand this format because it converts our react code in this format and show output on web
+// And react developers already define object format for running all codes and we create our own object
+
+// const ReactElement = {
+//     type: "a",
+//     props :{
+//         href: "https://google.com",
+//         target: "_blank",
+//     },
+//     children: "click me to visit google"
+// }
+
+// const anotherElement = (<a href="https://google.com" target="_blank">Click to visit google</a>)
+
+const anothetUser = "Ayush"
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    "Click me to visit google",
+    anothetUser
 )
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <App />
+
+);
